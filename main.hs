29012169d5b4af41b39e97d520825ad8488d30cd
pedrofavero <112567328+pedrofavero@users.noreply.github.com>
2025-11-09 15:24:33 -0300

    feat(main): adicionando tipos de dados

---
+++ main.hs
@@ -0,0 +1,50 @@
+module Main where
+
+import Data.Time (UTCTime)
+import qualified Data.Map as Map
+
+
+-- data cria um novo tipo de dado
+-- deriving (Show, Read) - para permitir a serialização e desserialização de/para o disco.
+data Item = Item
+  { itemID     :: String
+  , nome       :: String
+  , quantidade :: Int
+  , categoria  :: String
+  } deriving (Show, Read)
+
+-- para armazenar os itens.
+-- type nao cria um novo tipo, so fala pro compilador que 
+-- quando eu escrever Inventario ele vai interpretar como o tipo
+
+-- Map.Map cria uma estrutura chave valor
+-- string é o tipo da chave 
+-- Item é o tipo do valor
+type Inventario = Map.Map String Item
+
+-- ADT (Algebraic Data Type) — um tipo enumerado com várias formas possíveis.
+data AcaoLog
+  = Add
+  | Remove
+  | Update
+  | QueryFail
+  deriving (Show, Read)
+
+-- Representa o resultado de uma operação — sucesso ou erro.
+-- É um adt mas com variacao simples
+data StatusLog
+  = Sucesso
+  | Falha String
+  deriving (Show, Read)
+
+-- registro para o log
+data LogEntry = LogEntry
+  { timestamp :: UTCTime
+  , acao      :: AcaoLog
+  , detalhes  :: String
+  , status    :: StatusLog
+  } deriving (Show, Read)
+
+main :: IO ()
+main = do
+  putStrLn "tipos basicos"
